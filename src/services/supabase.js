0fbb446c16aaf2c589108f9db503f04ab2435b4a
Zelalem Getahun <zelalemg935@gmail.com>
2025-08-23 05:26:10 -0700

    supabase tables created

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://zzagsihxuohcxdftiydc.supabase.co";
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6YWdzaWh4dW9oY3hkZnRpeWRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2Mzc0ODgsImV4cCI6MjA3MTIxMzQ4OH0.sQk01kL93vOS2kiwOqc1YGsEk2UZxn1w6CkfjXrme7o`;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
