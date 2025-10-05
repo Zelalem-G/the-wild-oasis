cb26353983ba68233570535207dacfdbdffbc5c5
Zelalem Getahun <zelalemg935@gmail.com>
2025-10-05 11:23:56 -0700

    Authorization Protecting Routes

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Fullpage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { isLoading, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <Fullpage>
+        <Spinner />
+      </Fullpage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
