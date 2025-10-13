e845bef0ef9112ffa411272d78c349b8f1c19bb6
Zelalem Getahun <zelalemg935@gmail.com>
2025-10-13 10:18:10 -0700

    Displaying Stays for Current Day

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -15,12 +16,7 @@ const StyledDashboardLayout = styled.div`
 `;
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    numDays,
-    isLoading: isLoading2,
-  } = useRecentStays();
+  const { confirmedStays, numDays, isLoading: isLoading2 } = useRecentStays();
 
   const { cabins, isLoading: isLoading3 } = useCabins();
 
@@ -34,7 +30,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Todays activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
